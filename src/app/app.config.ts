5612d3a41cd2bbcf680554dcf704dd97d6fb733c
VIS-WAS <vishwas.ms8830@gmail.com>
2025-04-28 21:47:03 +0530

    added comment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+//added httpClient
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
